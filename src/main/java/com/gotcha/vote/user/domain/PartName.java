b84820ca3d615761e7081eca0d34b475547f7c19
jongmee <ra123221@gmail.com>
2023-12-16 21:25:53 +0900

    feat: 파트이름 enum에 디자이너와 기획자 추가

--- src/main/java/com/gotcha/vote/user/domain/PartName.java
+++ src/main/java/com/gotcha/vote/user/domain/PartName.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum PartName {
     FRONTEND("프론트엔드"),
-    BACKEND("백엔드");
+    BACKEND("백엔드"),
+    DESIGNER("디자이너"),
+    PRODUCT_PLANNER("기획자");
 
     private final String value;
 
