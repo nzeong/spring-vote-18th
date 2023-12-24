5ec42f8d3cf5c51d70c27e263cca605735287e53
nzeong <stac_cato@naver.com>
2023-12-24 18:10:26 +0900

    fix: 로그인 response body 반환값 수정

--- src/main/java/com/gotcha/vote/user/dto/response/TokenResponse.java
+++ src/main/java/com/gotcha/vote/user/dto/response/TokenResponse.java
@@ -1,5 +1,8 @@
 package com.gotcha.vote.user.dto.response;
 
+import com.gotcha.vote.user.domain.PartName;
+import com.gotcha.vote.user.domain.TeamName;
+import com.gotcha.vote.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,15 +11,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TokenResponse {
     private String accessToken;
+    private PartName partName;
+    private TeamName teamName;
 
     @Builder
-    public TokenResponse(String accessToken) {
+    public TokenResponse(String accessToken, PartName partName, TeamName teamName) {
         this.accessToken = accessToken;
+        this.partName = partName;
+        this.teamName = teamName;
     }
 
-    public static TokenResponse from(String accessToken) {
+    public static TokenResponse from(String accessToken, User selectedUser) {
         return TokenResponse.builder()
                 .accessToken(accessToken)
+                .partName(selectedUser.getPartName())
+                .teamName(selectedUser.getTeamName())
                 .build();
     }
 
